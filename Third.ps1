422c175d64f572be2ba2a103f9045046fe6f0e27
Renganathen <srenga527@apac.comcast.com>
2019-02-01 13:45:58 +0530

    script

---
+++ Third.ps1
@@ -0,0 +1,28 @@
+PowerShell
+Python
+AWS (Amazon Web Services)
+GitHub
+Database
+
+Tools:
+PowerShell ISE
+VisualStudioCode
+
+Online Training:
+Udemy.in
+https://www.packtpub.com/ --ebook
+
+
+Git commands:
+
+1. Create repository in github.
+2. git clone repolink.
+
+
+1. git add --all
+2. git commit -m "message"
+3. git push origin master
+
+
+1. git fetch origin
+2. git reset --hard origin/master
